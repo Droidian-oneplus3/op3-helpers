1caf8f17cb01f1c244acf678792a9b02032df693
Bardia Moshiri <fakeshell@bardia.tech>
2023-05-26 15:58:00 -0400

    fix the issue with tristate notifications not working on pulse disconnecting

--- tristate.c
+++ tristate.c
@@ -8,8 +8,8 @@
 #include <libnotify/notify.h>
 
 #define DEVICE_FILE "/dev/input/event1"
-
 #define NAME "tristate"
+#define NOTIFICATION_RESET_COUNT 10
 
 pa_mainloop *mainloop = NULL;
 
@@ -22,6 +22,73 @@ union volume_value {
 
 enum mute_action { MUTE_NO_ACTION, MUTE, UNMUTE, TOGGLE_MUTE };
 enum mute_action mute_action = MUTE_NO_ACTION;
+
+int notification_count = 0;
+
+typedef struct node {
+    char command[100];
+    struct node *next;
+} Node;
+
+Node *front = NULL;
+Node *rear = NULL;
+pthread_mutex_t lock;
+pthread_cond_t cond;
+
+void enqueue(char* command) {
+    Node *temp = (Node*)malloc(sizeof(Node));
+    strncpy(temp->command, command, sizeof(temp->command));
+    temp->next = NULL;
+
+    pthread_mutex_lock(&lock);
+
+    if (rear == NULL) {
+        front = rear = temp;
+    } else {
+        rear->next = temp;
+        rear = temp;
+    }
+
+    pthread_cond_signal(&cond);
+    pthread_mutex_unlock(&lock);
+}
+
+void* worker_thread(void *arg) {
+    while (1) {
+        pthread_mutex_lock(&lock);
+
+        while (front == NULL) {
+            pthread_cond_wait(&cond, &lock);
+        }
+
+        Node *temp = front;
+        front = front->next;
+
+        if (front == NULL) {
+            rear = NULL;
+        }
+
+        pthread_mutex_unlock(&lock);
+
+        // Execute the system command
+        system(temp->command);
+
+        free(temp);
+    }
+
+    return NULL;
+}
+
+void init_queue_and_worker() {
+    pthread_t tid;
+    pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&cond, NULL);
+
+    // Start the worker thread
+    pthread_create(&tid, NULL, worker_thread, NULL);
+}
+
+void toggle_mute(int mute_action);
 
 void on_success_quit(pa_context *context, int success, void *userdata) {
     pa_mainloop_quit(mainloop, 0);
@@ -62,6 +129,14 @@ void on_context_state_change(pa_context *context, void *userdata) {
     pa_context_state_t state = pa_context_get_state(context);
     if (state == PA_CONTEXT_READY) {
         pa_context_get_server_info(context, on_server_info, NULL);
+    } else if (state == PA_CONTEXT_FAILED || state == PA_CONTEXT_TERMINATED) {
+        printf("PulseAudio connection failed. Exiting the program.\n");
+        pa_context_unref(context);
+        if (mainloop) {
+            pa_mainloop_free(mainloop);
+            mainloop = NULL;
+        }
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -109,9 +184,12 @@ int main(int argc, char *argv[]) {
     char buffer[sizeof(struct inotify_event) + 255];
     ssize_t len;
 
+    init_queue_and_worker();
+
     while (1) {
         len = read(fd_notify, buffer, sizeof(buffer));
         if (len > 0) {
+            notification_count++;
             int fd_device = open(DEVICE_FILE, O_RDONLY);
             if (fd_device == -1) {
                 perror("Cannot access input device");
@@ -123,15 +201,15 @@ int main(int argc, char *argv[]) {
                 switch (ev.code) {
                     case 600:
                         unmute();
-                        system("pactl set-source-mute @DEFAULT_SOURCE@ 0");
+                        enqueue("pactl set-source-mute @DEFAULT_SOURCE@ 0");
                         break;
                     case 601:
                         mute();
-                        system("pactl set-source-mute @DEFAULT_SOURCE@ 0");
+                        enqueue("pactl set-source-mute @DEFAULT_SOURCE@ 0");
                         break;
                     case 602:
                         mute();
-                        system("pactl set-source-mute @DEFAULT_SOURCE@ 1");
+                        enqueue("pactl set-source-mute @DEFAULT_SOURCE@ 1");
                         break;
                     default:
                         break;
@@ -139,6 +217,34 @@ int main(int argc, char *argv[]) {
             }
 
             close(fd_device);
+
+            if (notification_count >= NOTIFICATION_RESET_COUNT) {
+                // Reset inotify
+                inotify_rm_watch(fd_notify, wd);
+                close(fd_notify);
+                notify_uninit();
+
+                // Reinitialize inotify
+                fd_notify = inotify_init();
+                if (fd_notify < 0) {
+                    printf("Error: inotify_init failed\n");
+                    return 1;
+                }
+
+                wd = inotify_add_watch(fd_notify, DEVICE_FILE, IN_MODIFY);
+                if (wd < 0) {
+                    printf("Error: inotify_add_watch failed\n");
+                    return 1;
+                }
+
+                if (!notify_init("tristate")) {
+                    fprintf(stderr, "Failed to reinitialize libnotify\n");
+                    return 1;
+                }
+
+                // Reset notification count
+                notification_count = 0;
+            }
         }
     }
 
